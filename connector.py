e510f51225e66bc1bc3235e4693341d89456a14e
sigmahlm <tobias@leichtdio.de>
2015-09-11 18:03:17 +0200

    Init Push

---
+++ connector.py
@@ -0,0 +1,101 @@
+"""
+	Connect to https://wordpress.org/plugins/json-api/
+"""
+
+import requests
+import json
+
+config = json.load(file("config.json", "r"))
+		
+class Connector:
+	"""
+		
+	"""
+	def __init__(self):
+		self.config = json.load(file("config.json", "r"))
+	
+	def test_pingloop (self):
+		pageno = 0
+		r = requests.get("%s?page=%s" %(config["meta"]["api"], pageno))
+		response = r.json()
+		while len(response) != 0:
+			
+			# Endloop
+			pageno += 1
+			r = requests.get("%s/posts?page=%s" %(config["meta"]["api"], pageno))
+			jsonfile = r.json()
+
+	def ping (self, reset=False):
+		"""
+			first ping : create db
+			next ping : update db
+		"""
+		#r = requests.get(config["meta"]["api"])
+		#jsonfile = r.json()
+		entrys = []
+		pageno = 0
+		url = "%s/posts?page=%s" %(config["meta"]["api"], pageno)
+		r = requests.get(url)
+		jsonfile = r.json()
+		
+		#print jsonfile
+		
+		while len(jsonfile) != 0:
+			print "Page No %s" %(pageno)
+			for entry in jsonfile:
+				cats = []
+				#print entry
+				for cat in entry["terms"]["category"]:		
+					cid = cat["ID"]
+					while cid > self.config["category"]["highest"]:
+						cid = cid - self.config["category"]["highest"]		
+					cats.append([cat["name"],
+								 cat["link"],
+								 self.config["category"][str(cid)]])
+		
+				try:
+					thumbnail = entry["featured_image"]["attachment_meta"]["sizes"]["thumbnail"]["url"]
+				except KeyError:
+					thumbnail = "http://placehold.it/42x42"
+				
+				new_entry = {
+					"ID" : entry["ID"],
+					"title" : entry["title"],
+					"url" : entry["link"],
+					"content" : entry["content"],
+					"img" : entry["featured_image"]["attachment_meta"]["sizes"]["medium"]["url"],
+					"thumb" : thumbnail,
+					"author" : {
+						"name" : entry["author"]["name"],
+						"avatar" : entry["author"]["avatar"],
+						"desc" : entry["author"]["description"]
+						},
+					"category" : cats
+					}
+				
+				entrys.append(new_entry)
+			# Endloop
+			pageno += 1
+			r = requests.get("%s/posts?page=%s" %(config["meta"]["api"], pageno))
+			jsonfile = r.json()
+		
+		json.dump(entrys, file("posts.json", "w"))
+	
+	def get_post(self, post_id):
+		jsonfile = json.load(file("posts.json", "r"))
+		for entry in jsonfile:
+			if entry["ID"] == post_id:
+				return entry
+		else:
+			return False
+	
+	def get_all_post(self):
+		jsonfile = json.load(file("posts.json", "r"))
+		return jsonfile
+
+
+if __name__ == "__main__":
+	debug = Connector()
+	#debug.initconf("http://dreifachglauben.de")
+	#debug.get_all_post()
+	debug.ping()
---
+++ main.py
@@ -0,0 +1,78 @@
+#! /usr/bin/python
+# coding: utf-8
+
+"""
+	Wordpress WebApp using bottle.py
+	and JSON Api ( https://wordpress.org/plugins/rest-api/ )
+	
+	
+"""
+
+from bottle import route, run, template, error, static_file, debug
+import json
+import connector
+
+
+@route('/css/<css>')
+def server_static(css):
+    return static_file(css, root="css")
+    
+@route('/fonts/<fontfile>')
+def server_static(fontfile):
+	return static_file(fontfile, root="fonts")
+
+@route('/js/<js_lib>')
+def server_static(js_lib):
+    return static_file(js_lib, root="js")
+
+@route('/images/<filename:re:.*\.png|.*\.jpg>')
+def send_image(filename):
+    if ".jpg" in filename:
+        return static_file(filename, root="images", mimetype="image/jpeg")
+    elif ".png" in filename:
+        return static_file(filename, root='images', mimetype='image/png')
+
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
+
+
+@route('/article/<article_id>')
+def article(article_id):
+	debug = connector.Connector()
+	postdata = debug.get_post(int(article_id))
+	config = json.load(file("config.json", "r"))
+	attrb = {"meta" : config["meta"],
+			 "social" : config["social"],
+			 "sharelinks" : config["sharelinks"],
+			 "entry" : postdata,
+	}
+	attrb["meta"]["url"] = postdata["url"]
+	print config["meta"]
+	
+	return template("blog_article.html", attrb)
+
+@route('/ping/<passwd>')
+def update(passwd):
+	config = json.load(file("config.json", "r"))
+	if passwd == config["pingpasswd"]:
+		debug = connector.Connector()
+		debug.ping()
+		return "Done."
+	return "None."
+
+@route('/')
+def blogwebapp():
+	config = json.load(file("config.json", "r"))
+	attrb = {"meta" : config["meta"],
+			 "social" : config["social"],
+			 "sharelinks" : config["sharelinks"],
+			 "url" : config["meta"]["url"],
+	}
+	debug = connector.Connector()
+	attrb["entrys"] = debug.get_all_post()
+	return template("blog_rachet.html", attrb)
+
+
+debug(True)
+run(host='192.168.1.162', port=8000, debug=False, reloader=True)
